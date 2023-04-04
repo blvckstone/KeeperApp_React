5be733cdf5e4e6a48b366b48c757d53b38db608d
Blvckstone <mohammadshoiab9011@gmail.com>
2023-04-04 21:56:41 +0530

    Reusable card componenet and header ui with tailwind

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import Header from './components/Header';
+import Card from './components/Card';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Header />
+      <Card title="Noteworthy technology acquisitions 2021" content="Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order."/>
+      <Card title="Noteworthy technology acquisitions 2021" content="Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order."/>
+      
+      
     </div>
   );
 }
